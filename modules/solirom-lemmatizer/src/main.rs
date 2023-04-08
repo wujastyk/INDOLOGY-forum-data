65bbd662540122a51cc2a1a25f71c23e97904f5a
claudius <claudius.teodorescu@gmail.com>
2023-04-08 14:13:22 +0300

    Added solirom-lemmatizer module

---
+++ modules/solirom-lemmatizer/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// time cargo build --bin text-data-to-index-cards --target-dir ./target --release --target x86_64-unknown-linux-musl
+// time cargo run --bin text-data-to-index-cards --target-dir ./target -- "/home/claudius/workspace/repositories/git/github.com/wujastyk/INDOLOGY-forum-data/data/**/*.txt"
+// time ./text-data-to-index-cards "/home/claudius/workspace/repositories/git/github.com/wujastyk/INDOLOGY-forum-data/data/**/*.txt"
