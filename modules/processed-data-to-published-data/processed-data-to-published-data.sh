966d6b8578152907981b35139d9b484e526a3ab0
claudius <claudius.teodorescu@gmail.com>
2023-04-12 12:17:28 +0300

    Generated the published data

--- modules/processed-data-to-published-data/processed-data-to-published-data.sh
+++ modules/processed-data-to-published-data/processed-data-to-published-data.sh
@@ -3,14 +3,15 @@
 base_data_dir=$(realpath "data")
 
 cp "$base_data_dir/01-00003.txt" "$base_data_dir/_test"
-find "$base_data_dir/_test" -name "*.txt" -exec sed -i -e \
-    '/^ *$\|^>\|^--\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. /d' {} \;
-find "$base_data_dir/_test" -name "*.txt" -exec sed -i 's|Subject: ||g' {} \;
-#find "$base_data_dir/_test" -name "*.txt" -exec sed -i -e '/^Message-ID: /d' {} \;
 
-#for i in {1990..2022}
-#do
-    #current_data_dir=$(realpath "$base_data_dir/$i")
+for i in {1990..2022}
+do
+    current_data_dir=$(realpath "$base_data_dir/$i")
     
-    # find $current_data_dir -name "*.txt" -exec sed -e '/^$/d' {} \;
-#done
+    for file in "$current_data_dir/*.txt"
+    do
+        sed -i -e \
+            '/^\s*$\|^ *$\|^>\|^--\|^__\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. \|was scrubbed...\|^Name: \|^Type: \|^Size: \|^Desc: \|^URL: /d' $file
+        sed -i 's|Subject: ||g' $file        
+    done
+done
