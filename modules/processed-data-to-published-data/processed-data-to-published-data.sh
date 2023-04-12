bb067c0051dca58a03c87c29fc9d282332e55c23
claudius <claudius.teodorescu@gmail.com>
2023-04-12 11:50:21 +0300

    Added module processed-data-to-published-data

--- modules/processed-data-to-published-data/processed-data-to-published-data.sh
+++ modules/processed-data-to-published-data/processed-data-to-published-data.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
-base_data_dir="/home/claudius/varia/Dominik/data"
+base_data_dir=$(realpath "data")
 
-for i in {1990..2022}
-do
-    current_data_dir="$base_data_dir/$i"
+cp "$base_data_dir/01-00003.txt" "$base_data_dir/_test"
+find "$base_data_dir/_test" -name "*.txt" -exec sed -i -e \
+    '/^ *$\|^>\|^--\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. /d' {} \;
+find "$base_data_dir/_test" -name "*.txt" -exec sed -i 's|Subject: ||g' {} \;
+#find "$base_data_dir/_test" -name "*.txt" -exec sed -i -e '/^Message-ID: /d' {} \;
+
+#for i in {1990..2022}
+#do
+    #current_data_dir=$(realpath "$base_data_dir/$i")
     
-    find "$current_data_dir" -name "*.gz" -exec gunzip -d {} \;
-    
-    find "$current_data_dir" -name "*January.txt" -exec mv {} "$current_data_dir/01.txt" \;
-    find "$current_data_dir" -name "*February.txt" -exec mv {} "$current_data_dir/02.txt" \;
-    find "$current_data_dir" -name "*March.txt" -exec mv {} "$current_data_dir/03.txt" \;
-    find "$current_data_dir" -name "*April.txt" -exec mv {} "$current_data_dir/04.txt" \;
-    find "$current_data_dir" -name "*May.txt" -exec mv {} "$current_data_dir/05.txt" \;
-    find "$current_data_dir" -name "*June.txt" -exec mv {} "$current_data_dir/06.txt" \;
-    find "$current_data_dir" -name "*July.txt" -exec mv {} "$current_data_dir/07.txt" \;
-    find "$current_data_dir" -name "*August.txt" -exec mv {} "$current_data_dir/08.txt" \;
-    find "$current_data_dir" -name "*September.txt" -exec mv {} "$current_data_dir/09.txt" \;
-    find "$current_data_dir" -name "*October.txt" -exec mv {} "$current_data_dir/10.txt" \;
-    find "$current_data_dir" -name "*November.txt" -exec mv {} "$current_data_dir/11.txt" \;
-    find "$current_data_dir" -name "*December.txt" -exec mv {} "$current_data_dir/12.txt" \;
-    
-    for file in "$current_data_dir"/*.txt; do
-        file_name=${file##*/}
-        file_stem_name=${file_name%.txt}
-        file_prefix="$current_data_dir"/"$file_stem_name"-
-        csplit -s -z "$file" --prefix "$file_prefix" --suffix "%05d.txt" --elide-empty-files '/From /' '{*}'
-        rm "$file"        
-    done
-done
+    # find $current_data_dir -name "*.txt" -exec sed -e '/^$/d' {} \;
+#done
