73c4ccb3e270410e5d3f028cb66f9bf4486c7188
claudius <claudius.teodorescu@gmail.com>
2023-05-07 20:48:10 +0300

    Improved conversion from processed to published data

--- modules/processed-data-to-published-data/processed-data-to-published-data.sh
+++ modules/processed-data-to-published-data/processed-data-to-published-data.sh
@@ -9,7 +9,7 @@ do
     for file in "$current_data_dir/*.txt"
     do
         sed -i -e \
-            '/^\s*$\|^ *$\|^>\|^--\|^__\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. \|was scrubbed...\|^Name: \|^Type: \|^Size: \|^Desc: \|^URL: \|^References: /d' $file
+            '/^\s*$\|^ *$\|^>\|^--\|^__\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. \|was scrubbed...\|^Name: \|^Type: \|^Size: \|^Desc: \|^URL: \|^References: \|https:\/\/wetransfer.com\//d' $file
         sed -i 's|Subject: ||g' $file        
     done
 done
