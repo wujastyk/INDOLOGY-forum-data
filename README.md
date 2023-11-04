069f1b54156192e7c4f75c76fc37145b25c4e81a
claudius <claudius.teodorescu@gmail.com>
2023-11-04 20:02:34 +0200

    Improved the build

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Steps for adding new data
 
-1. Download the archive for one month from the mailing list.
-2. Unarchive the archive and store its contents in the folder __/input__.
-3. Repeat the above steps for any new archives.
-3. Commit the changes.
+1. Delete all files from the __/input__ folder. 
+2. Download the archive for one month from the mailing list and store the file it contains in the __/input__ folder. You can process multiple files at once.
+3. Check if the file names follows the convention: __YEAR-Month.txt__ (e.g. __2023-January.txt__), and change them, if necessary.
+4. Commit the changes.
--- modules/publish-data/publish-data.sh
+++ modules/publish-data/publish-data.sh
@@ -1,9 +1,55 @@
 
 GITHUB_WORKSPACE=$(pwd)
-rm -rf "$GITHUB_WORKSPACE/public"
+input_dir_path=$GITHUB_WORKSPACE/input
+data_dir_path=$GITHUB_WORKSPACE/data
 
-# the part below goes into Github workflows
+rm -rf "$GITHUB_WORKSPACE/public"
 mkdir -p "$GITHUB_WORKSPACE/public"
+
+# unarchive the archives from the 'input' folder
+#find "$input_dir_path" -name "*.gz" -exec gunzip -d {} \;
+
+# rename each file from 'input' folder and move it to a subfolder with year as name
+declare -A months
+months=([January]=01 [February]=02 [March]=03 [April]=04 [May]=05 [June]=06 [July]=07 [August]=08 [September]=09 [October]=10 [November]=11 [December]=12)
+for file in $input_dir_path/*.txt; do
+    file_name=${file##*/}
+    file_stem_name=${file_name%.txt}
+    IFS=- read -r year month_name <<< $file_stem_name
+    month_number=${months[$month_name]}
+    new_file_name=$year-$month_number.txt
+    mv $file $input_dir_path/$new_file_name
+done
+
+# split each file in the 'input' into messages
+for file in $input_dir_path/*.txt; do
+    file_name=${file##*/}
+    file_stem_name=${file_name%.txt}
+    IFS=- read -r year month_number <<< $file_stem_name
+    mkdir $input_dir_path/$year
+    file_prefix=$input_dir_path/$year/$month_number-   
+    echo $file_prefix
+    csplit -s -z "$file" --prefix "$file_prefix" --suffix "%05d.txt" --elide-empty-files '/From /' '{*}'
+    rm "$file"
+done
+
+# process every file in the 'input' folder
+for file in "$input_dir_path/**/*.txt"; do
+    sed -i -e \
+        '/^\s*$\|^ *$\|^>\|^--\|^__\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. \|was scrubbed...\|^Name: \|^Type: \|^Size: \|^Desc: \|^URL: \|^References: \|https:\/\/wetransfer.com\/\|=\?utf\-8?B\?/d' $file
+    sed -i 's|Subject: ||g' $file
+done
+
+# copy all the files in 'input' folder to 'data' folder
+for folder in "$input_dir_path/**"; do
+    folder_name=$(basename $folder)
+    target_dir_path=$data_dir_path/$folder_name
+    mkdir $target_dir_path
+    cp $folder/*.txt $target_dir_path/
+    rm -rf $folder
+done
+
+# copy the data to the 'public' folder
 public_dir_absolute_path="$(realpath $GITHUB_WORKSPACE/public)"
 cp -r "$GITHUB_WORKSPACE/data/" "$GITHUB_WORKSPACE/public"
 
