2366d5f0ac0d2a8e3fe8c9847822e09bd1cd7257
claudius <claudius.teodorescu@gmail.com>
2023-11-15 14:42:20 +0200

    Improved the build

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Steps for adding new data
 
-1. Delete all folders and files from the __/input__ folder. 
-2. Download the archive for one month from the mailing list, unarchive
-it and store the file it contains in the __/input__ folder. You can process multiple files at once.
-3. Check if the file names in the __/input__ folder follows the convention: __YEAR-Month.txt__ (e.g. __2023-January.txt__), and change them, if necessary.
-4. Commit the changes.
+1. Download the archive for one month from the mailing list, and store the file it contains in the __/archives/YEAR__ folder. Create the folder for a new year, if needed. You can process multiple files at once.
+2. Check if the new archive's name follows the convention: __YEAR-Month.txt.gz__ (e.g. __2023-January.txt.gz__). In case if they are not, contact the developer.
+3. Commit the changes.
--- modules/publish-data/publish-data.sh
+++ modules/publish-data/publish-data.sh
@@ -3,62 +3,53 @@ GITHUB_WORKSPACE=$(pwd)
 archives_dir_path=$GITHUB_WORKSPACE/archives
 public_dir_path=$GITHUB_WORKSPACE/public
 raw_data_dir_path=$public_dir_path/raw-data
+data_dir_path=$public_dir_path/data
 
 input_dir_path=$GITHUB_WORKSPACE/input
-data_dir_path=$GITHUB_WORKSPACE/data
-raw_data_dir_path=$GITHUB_WORKSPACE/raw-data
 
 rm -rf $public_dir_path
 mkdir -p $public_dir_path
 mkdir -p $raw_data_dir_path
+mkdir -p $data_dir_path
 
 # copy the archives to the 'public/raw-data' folder
 cp -a $archives_dir_path/. $raw_data_dir_path/
 
-# unarchive the archives from the 'input' folder
+# unarchive the archives from the 'public/raw-data' folder
 find "$raw_data_dir_path" -name "*.gz" -exec gunzip -d {} \;
-exit 0;
-# rename each file from 'input' folder and move it to a subfolder with year as name
+
+# rename each file from the 'public/raw-data' folder and move it to a subfolder with year as name
 declare -A months
 months=([January]=01 [February]=02 [March]=03 [April]=04 [May]=05 [June]=06 [July]=07 [August]=08 [September]=09 [October]=10 [November]=11 [December]=12)
-for file in $input_dir_path/*.txt; do
+for file in $raw_data_dir_path/**/*.txt; do
     file_name=${file##*/}
     file_stem_name=${file_name%.txt}
     IFS=- read -r year month_name <<< $file_stem_name
     month_number=${months[$month_name]}
-    new_file_name=$month_number.txt
-    mkdir -p $input_dir_path/$year
-    mv $file $input_dir_path/$year/$new_file_name
+    new_file_name=$year-$month_number.txt
+    mkdir -p $raw_data_dir_path/$year
+    mv $file $raw_data_dir_path/$year/$new_file_name
 done
 
-# split each file in the 'input' into messages
-for file in $input_dir_path/**/*.txt; do
+# split each file in the 'public/raw-data' into messages
+for file in $raw_data_dir_path/**/*.txt; do
     file_name=${file##*/}
     file_stem_name=${file_name%.txt}
-    file_prefix=$input_dir_path/$year/$file_stem_name-   
-    echo $file_prefix
+    IFS=- read -r year month_number <<< $file_stem_name
+    file_prefix=$raw_data_dir_path/$year/$month_number-
     csplit -s -z "$file" --prefix "$file_prefix" --suffix "%05d.txt" --elide-empty-files '/From /' '{*}'
     rm "$file"
 done
 
-# copy all the files from 'input' folder to 'raw-data' folder
-cp -a $input_dir_path/. $raw_data_dir_path/
+# copy all the files from 'public/raw-data' folder to 'public/data' folder
+cp -a $raw_data_dir_path/. $data_dir_path/
 
-# process every file in the 'input' folder
-for file in "$input_dir_path/**/*.txt"; do
+# process every file in the 'public/data' folder
+time for file in $data_dir_path/**/*.txt; do
     sed -i -e \
         '/^\s*$\|^ *$\|^>\|^--\|^__\|^From[:]\{0,1\} \|^To: \|^Cc: \|^Date: \|^Message-ID: \|^Message-Id: \|^In-Reply-To: \|^Reply-To: \|^Received: \|^Return-path: \|^Phone \|^Tel: \|^Tel. \|was scrubbed...\|^Name: \|^Type: \|^Size: \|^Desc: \|^URL: \|^References: \|https:\/\/wetransfer.com\/\|=\?utf\-8?B\?/d' $file
     sed -i 's|Subject: ||g' $file
 done
-
-# copy all the processed files in 'input' folder to 'data' folder
-cp -a $input_dir_path/. $data_dir_path/
-
-# copy the raw data to the 'public' folder
-cp -r "$GITHUB_WORKSPACE/raw-data/" "$GITHUB_WORKSPACE/public"
-
-# copy the data to the 'public' folder
-cp -r "$GITHUB_WORKSPACE/data/" "$GITHUB_WORKSPACE/public"
 
 # generate the triples
 time "$GITHUB_WORKSPACE/modules/text-data-to-triples/text-data-to-triples" "$GITHUB_WORKSPACE/data/" "**/*.txt" "$GITHUB_WORKSPACE/public/datasets/" "https://wujastyk.github.io/INDOLOGY-forum-data/indexes/words-fulltext/" "https://wujastyk.github.io/INDOLOGY-forum-data/data/"
